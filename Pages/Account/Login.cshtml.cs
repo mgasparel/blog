6a041acab6da4ec7876c9df7412f17906aea866a
Mike Gasparelli <magasparel@gmail.com>
2017-09-11 23:44:19 -0400

    Add Login page

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using blog.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace blog.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(
+            SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager,
+            ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+
+            _userManager = userManager;
+
+            _logger  = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public bool NoIdentitiesExist { get; set; }
+
+        public class InputModel
+        {
+            [Required, EmailAddress]
+            public string Email { get; set; }
+
+            [Required, DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            NoIdentitiesExist = _userManager.Users.Count() == 0;
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl)
+        {
+            ReturnUrl = returnUrl;
+
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                if(result.Succeeded)
+                {
+                    _logger.LogInformation("{Email} logged in.", Input.Email);
+
+                    if(Url.IsLocalUrl(returnUrl))
+                    {
+                        return LocalRedirect(returnUrl);
+                    }
+
+                    return LocalRedirect(Url.Page("/Index"));
+                }
+
+                if(result.IsLockedOut)
+                {
+                    _logger.LogWarning("{Email} attempted to log in with locked account", Input.Email);
+
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    
+                    return Page();
+                }
+            }
+
+            return Page();
+        }
+    }
+}
