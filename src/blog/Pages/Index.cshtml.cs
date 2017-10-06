193ff344c9c352f9f9baaab1eb8b4d1d6d7b2434
Mike Gasparelli <magasparel@gmail.com>
2017-10-05 20:40:06 -0400

    Add all posts on home page

--- src/blog/Pages/Index.cshtml.cs
+++ src/blog/Pages/Index.cshtml.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blog.Data;
+using blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace blog.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IEnumerable<Post> Posts { get; set; }
+
+        public IndexModel(ApplicationDbContext db, ILogger<IndexModel> logger)
+        {
+            _db = db;
+
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            Posts = _db.Posts
+                .Include(x => x.PostTags)
+                .ThenInclude(x => x.Tag)
+                .Include(p => p.Author)
+                .ToList();
         }
     }
 }
